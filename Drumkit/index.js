2ed72e822e7b135b5fa8727fcbbbee2109af0c67
Karlee-lewis <60264743+Karlee-lewis@users.noreply.github.com>
2021-02-04 12:35:31 -0700

    added javascript functionality
    case statements added for buttons

--- Drumkit/index.js
+++ Drumkit/index.js
@@ -1,13 +1,49 @@
-
-
 var numOfButtons = document.querySelectorAll(".drum").length;
 var buttonArray = document.querySelectorAll(".drum");
-var audio = new Audio('sounds/tom-1.mp3');
+
 
 for (var i = 0; i < numOfButtons; i++) {
-  buttonArray[i].addEventListener("click", function (){
-    console.log.(this);
-    //--- What to do when clicked
+  buttonArray[i].addEventListener("click", function() {
+    var buttonInnerHTML = this.innerHTML;
+    switch (buttonInnerHTML) {
+      case "w":
+        var tom1 = new Audio('sounds/tom-1.mp3');
+        tom1.play();
+        break;
+
+      case "a":
+        var tom2 = new Audio('sounds/tom-2.mp3');
+        tom2.play();
+        break;
+
+      case "s":
+        var snare = new Audio('sounds/snare.mp3');
+        snare.play();
+        break;
+
+      case "d":
+        var kickBass = new Audio('sounds/kick-bass.mp3');
+        kickBass.play();
+        break;
+
+      case "j":
+        var tom3 = new Audio('sounds/tom-3.mp3');
+        tom3.play();
+        break;
+
+      case "k":
+        var cymbal = new Audio('sounds/crash.mp3');
+        cymbal.play();
+        break;
+
+      case "l":
+        var tom4 = new Audio('sounds/tom-4.mp3');
+        tom4.play();
+        break;
+
+      default:
+        console.log(buttonInnerHTML)
+    }
   });
 }
 
